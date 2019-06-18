1f31c6237a04db1db0c03abdea59381884d489ba
Lucas Licursi <llicursi@gmail.com>
2019-06-18 19:24:00 +0200

    Inits maven-wrapper and spring-boot with basic web and jpa.

---
+++ src/main/java/com/llicursi/opto/OptoApplication.java
@@ -0,0 +1,13 @@
+package com.llicursi.opto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llicursi/opto/OptoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.llicursi.opto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OptoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
