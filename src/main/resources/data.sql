030d1b79fc5d037eb6fd5f52a20ed02ac739bd7d
Lucas Licursi <llicursi@gmail.com>
2019-06-19 16:56:12 +0200

    Initial database user and reduce security log

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Create 4 user permission
+
+insert into users (user_id, name, surname, email, password, role) values (00001, 'User', 'User', 'user', '$2a$06$2qeXm6qDiaLtHN26Blt.9.5Waeo9HBQSFrYrrS7/MHiQPrnxT/Bci', 'USER');
+insert into users (user_id, name, surname, email, password, role) values (00002, 'Admin', 'User', 'admin', '$2a$06$2qeXm6qDiaLtHN26Blt.9.5Waeo9HBQSFrYrrS7/MHiQPrnxT/Bci', 'ADMIN');
