0d7aeb1e6c0e9c361a3562e206e3441617ee3719
Lucas Licursi <llicursi@gmail.com>
2019-06-24 19:39:11 +0200

    Breaks test data per table

--- src/test/resources/db/migration/V1.0.1.1__include_users.sql
+++ src/test/resources/db/migration/V1.0.1.1__include_users.sql
@@ -19,10 +19,6 @@ values (6, 'Marek', 'Zuadi', 'marek@opto.com', '$2a$06$2qeXm6qDiaLtHN26Blt.9.5Wa
 insert into user (id,  name, surname, email, password, role)
 values (7, 'Pinus', 'Mole', 'pinus@opto.com', '$2a$06$2qeXm6qDiaLtHN26Blt.9.5Waeo9HBQSFrYrrS7/MHiQPrnxT/Bci', 'USER') ;
 
-insert into subject ( user_id, title, description, start, due, active) values ( 1, 'Topic 1', '', DATEADD(DAY, -3, CURRENT_TIMESTAMP ), DATEADD(DAY, 4, CURRENT_TIMESTAMP ), true);
-insert into subject ( user_id, title, description, start, due, active) values ( 3, 'Topic 2 Inactive', '', DATEADD(DAY, -3, CURRENT_TIMESTAMP ), DATEADD(DAY, 4, CURRENT_TIMESTAMP ), false);
-insert into subject ( user_id, title, description, start, due, active) values ( 1, 'Topic 3 Expired', '', DATEADD(DAY, -15, CURRENT_TIMESTAMP ), DATEADD(DAY, -4, CURRENT_TIMESTAMP ), true);
-
 -- drop schema opto;
 -- create schema opto;
 
