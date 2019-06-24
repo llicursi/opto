5528c15e4bd30585110969a986c2586525e06ffa
Lucas Licursi <llicursi@gmail.com>
2019-06-24 23:13:37 +0200

    Add specific response status for InvalidDateRangeException

--- src/main/java/com/llicursi/opto/exception/InvalidDateRangeException.java
+++ src/main/java/com/llicursi/opto/exception/InvalidDateRangeException.java
@@ -3,6 +3,7 @@ package com.llicursi.opto.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidDateRangeException extends Exception {
 
     static final long serialVersionUID = -6687526993444229950L;
